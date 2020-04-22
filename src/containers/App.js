c373bed70b31da58e1037fb3460b37aa848b3e84
sanket_jain <sanketjain1117@gmail.com>
2020-04-23 00:18:41 +0530

    test

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,6 +18,7 @@ class App extends Component {
     }
 
     componentDidMount(){
+        
         fetch('https://jsonplaceholder.typicode.com/users')
           .then(response => response.json())
           .then(users => {this.setState({robots: users})});
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provide,connect } from 'react-redux';
+import {createStore} from 'redux';
 //import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 //import {robots} from './robots';
 import 'tachyons';
 import App from './containers/App';
-//import SearchBox from './SearchBox'; 
+import {searchRobots} from './reducers'; 
 
-ReactDOM.render(<App/> ,document.getElementById('root')
-);
+
+const store = createStore(searchRobots); 
+
+ReactDOM.render(
+                <provider store={store}>
+                   <App /> 
+                </provider> ,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
